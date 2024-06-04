3fd7e4c19186108cf2c7f7792fa7a84868bae941
Alex C <acoe444@gmail.com>
2024-06-04 15:15:18 +0100

    completed 3sum

---
+++ solutions_python/three_sum.py
@@ -0,0 +1,24 @@
+import copy
+
+def three_sum(nums):
+    my_dict = {}
+    result_set = set()
+    result = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums) - 1):
+            sum = nums[i] + nums[j]
+            neg = 0 - sum
+            # if neg not in my_dict:
+            my_dict[neg] = [nums[i], nums[j]]
+            if nums[j + 1] in my_dict:
+                triplet = copy.deepcopy(my_dict[nums[j+1]])
+                triplet.append(nums[j+1])
+                triplet.sort()
+                tup = tuple(triplet)
+                result_set.add(tup)
+            print(my_dict)
+        my_dict.clear()
+
+    for val in result_set:
+        result.append(list(val))
+    return result
