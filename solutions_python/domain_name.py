522c897ef9d10679932a4492f9b1ec8824bba2d9
Alex C <acoe444@hotmail.com>
2024-06-20 11:02:31 +0100

    refactored domain_name slightly

--- solutions_python/domain_name.py
+++ solutions_python/domain_name.py
@@ -2,7 +2,6 @@ def get_domain_name(url):
     new_str = url.replace('http://', '')
     new_str = new_str.replace('https://', '')
     new_str = new_str.replace('www.', '')
-    new_str = new_str.split(".")[0]
+    result = new_str.split(".")[0]
     
-    result = new_str
     return result
