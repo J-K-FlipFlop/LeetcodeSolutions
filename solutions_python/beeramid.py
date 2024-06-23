e0dc4d13f921ec91b365d41b429bfa78ce2ec11f
Alex C <acoe444@hotmail.com>
2024-06-23 23:15:50 +0100

    completed beeramid

---
+++ solutions_python/beeramid.py
@@ -0,0 +1,14 @@
+def beeramid(money, price):
+    i = 0
+    total_beers = 0
+
+    if money <= 0:
+        return 0
+
+    while total_beers * price < money:
+        i += 1
+        total_beers += i**2
+    
+    return i - 1
+
+
