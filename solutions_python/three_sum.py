fb10626af0f5ebf4d2da6ae41771c2f9dd886167
Alex C <acoe444@hotmail.com>
2024-08-28 23:48:59 +0100

    refactored three_sum

--- solutions_python/three_sum.py
+++ solutions_python/three_sum.py
@@ -4,11 +4,11 @@ def three_sum(nums):
     my_dict = {}
     result_set = set()
     result = []
+
     for i in range(len(nums)):
         for j in range(i + 1, len(nums) - 1):
             sum = nums[i] + nums[j]
             neg = 0 - sum
-            # if neg not in my_dict:
             my_dict[neg] = [nums[i], nums[j]]
             if nums[j + 1] in my_dict:
                 triplet = copy.deepcopy(my_dict[nums[j+1]])
@@ -16,7 +16,6 @@ def three_sum(nums):
                 triplet.sort()
                 tup = tuple(triplet)
                 result_set.add(tup)
-            print(my_dict)
         my_dict.clear()
 
     for val in result_set:
